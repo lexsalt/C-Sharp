10d05b768b93927ccb569824b9dd2464a695a199
lexsalt <emilianosal@hotmail.com>
2023-10-24 18:25:27 +0200

    17th test

--- Program.cs
+++ Program.cs
@@ -512,27 +512,140 @@ fraudulentOrderIDs[2] = "C789";
 // Console.ReadLine();
 
 // 17th test
-int[] numbers = { 4, 8, 15, 16, 23, 42 };
-int total = 0;
-bool found = false;
-foreach (int number in numbers)
+// int[] numbers = { 4, 8, 15, 16, 23, 42 };
+// int total = 0; //line added for the task 
+// bool found = false; //line addedd for fulfillment of the task
+
+// foreach (int number in numbers)
+// {
+//     // int total;
+
+//     total += number;
+
+//     if (number == 42)
+//     {
+//        found = true;
+
+//     }
+
+// }
+
+// if (found) 
+// {
+//     Console.WriteLine("Set contains 42");
+
+// }
+
+// Console.WriteLine($"Total: {total}");
+
+// 18th test: switch statement
+
+// string fruit = "apple";
+
+// switch (fruit)
+// {
+//     case "apple":
+//         Console.WriteLine($"App will display information for apple.");
+//         break;
+
+//     case "banana":
+//         Console.WriteLine($"App will display information for banana.");
+//         break;
+
+//     case "cherry":
+//         Console.WriteLine($"App will display information for cherry.");
+//         break;
+// }
+
+// second code block 
+
+// int employeeLevel = 200;
+// string employeeName = "John Smith";
+
+// string title = "";
+
+// switch (employeeLevel)
+// {
+//     case 100:
+//         title = "Junior Associate";
+//         break;
+//     case 200:
+//         title = "Senior Associate";
+//         break;
+//     case 300:
+//         title = "Manager";
+//         break;
+//     case 400:
+//         title = "Senior Manager";
+//         break;
+//     default:
+//         title = "Associate";
+//         break;
+// }
+
+// Console.WriteLine($"{employeeName}, {title}");
+
+// 20th test: change if statements for switch
+
+// SKU = Stock Keeping Unit. 
+// SKU value format: <product #>-<2-letter color code>-<size code>
+// string sku = "01-MN-L";
+
+// string[] product = sku.Split('-');
+
+// string type = "";
+// string color = "";
+// string size = "";
+
+// switch (product[0])
+// {
+//     case "01":
+//         type = "Sweat shirt";
+//         break;
+//     case "02":
+//         type = "T-Shirt";
+//         break;
+//     case "03":
+//         type = "Sweat pants";
+//         break;
+//     default:
+//         type = "Other";
+//         break;
+// }
+
+// switch (product[1])
+// {
+//     case "BL":
+//         color = "Black";
+//         break;
+//     case "MN":
+//         color = "Maroon";
+//         break;
+//     default:
+//         color = "White";
+//         break;
+// }
+// switch (product[2])
+// {
+//     case "S":
+//         size = "Small";
+//         break;
+//     case "M":
+//         size = "Medium";
+//         break;
+//     case "L":
+//         size = "Large";
+//         break;
+//     default:
+//         size = "One Size Fits All";
+//         break;
+// }
+
+// Console.WriteLine($"Product: {size} {color} {type}");
+
+// 21th test
+
+for (int i = 0; i < 10; i++)
 {
-    // int total;
-
-    total += number;
-
-    if (number == 42)
-    {
-       found = true;
-
-    }
-
+    Console.WriteLine(i);
 }
-
-if (found) 
-{
-    Console.WriteLine("Set contains 42");
-
-}
-
-Console.WriteLine($"Total: {total}");
