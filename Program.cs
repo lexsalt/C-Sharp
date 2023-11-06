c6f21b550f9a2eb86394c54062ee4b45a00672fd
lexsalt <emilianosal@hotmail.com>
2023-11-06 19:59:44 +0100

    30th test. Resume from macbook

--- Program.cs
+++ Program.cs
@@ -723,27 +723,122 @@ Console.WriteLine($"Last number: {current}");
 
 // 26th test 
 
-int[] schedule = {800, 1200, 1600, 2000};
+// int[] schedule = {800, 1200, 1600, 2000};
 
-void DisplayAdjustedTimes(int[] times, int currentGMT, int newGMT) 
+// void DisplayAdjustedTimes(int[] times, int currentGMT, int newGMT) 
+// {
+// int diff = 0;
+// if (Math.Abs(newGMT) > 12 || Math.Abs(currentGMT) > 12)
+// {
+//     Console.WriteLine("Invalid GMT");
+// }
+// else if (newGMT <= 0 && currentGMT <= 0 || newGMT >= 0 && currentGMT >= 0) 
+// {
+//     diff = 100 * (Math.Abs(newGMT) - Math.Abs(currentGMT));
+// } 
+// else 
+// {
+//     diff = 100 * (Math.Abs(newGMT) + Math.Abs(currentGMT));
+// }
+// for (int i = 0; i < times.Length; i++) 
+// {
+//     int newTime = ((times[i] + diff)) % 2400;
+//     Console.WriteLine($"{times[i]} -> {newTime}");
+// }
+// }
+// DisplayAdjustedTimes(schedule, 6, -6);
+
+
+// 26th test: method scope
+
+// string[] students = {"Jenna", "Ayesha", "Carlos", "Viktor"};
+
+// DisplayStudents(students);
+// DisplayStudents(new string[] {"Robert","Vanya"});
+
+// void DisplayStudents(string[] students) 
+// {
+//     foreach (string student in students) 
+//     {
+//         Console.Write($"{student}, ");
+//     }
+//     Console.WriteLine();
+// }
+
+// Notice that the method parameter student takes precedence over the global student array. It's important to be deliberate about what global variables you want your methods to use.
+
+//27th test
+
+// double pi = 3.14159;
+// PrintCircleInfo(12);
+// PrintCircleInfo(24);
+
+
+// void PrintCircleArea(int radius)
+// {
+//     double area = pi * (radius * radius);
+//     Console.WriteLine($"Area = {area}");
+// }
+
+// void PrintCircleCircumference(int radius)
+// {
+//     double circumference = 2 * pi * radius;
+//     Console.WriteLine($"Circumference = {circumference}");
+// }
+// void PrintCircleInfo(int radius) 
+// {
+//     Console.WriteLine($"Circle with radius {radius}");
+//     PrintCircleArea(radius);
+//     PrintCircleCircumference(radius);
+// }
+
+// 28th test
+// int a = 3;
+// int b = 4;
+// int c = 0;
+
+// Multiply(a, b, c);
+// Console.WriteLine($"global statement: {a} x {b} = {c}");
+
+// void Multiply(int a, int b, int c) 
+// {
+//     c = a * b;
+//     Console.WriteLine($"inside Multiply method: {a} x {b} = {c}");
+// }
+
+//  29th test
+// int[] array = {1, 2, 3, 4, 5};
+
+// PrintArray(array);
+// Clear(array);
+// PrintArray(array);
+
+// void PrintArray(int[] array) 
+// {
+//     foreach (int a in array) 
+//     {
+//         Console.Write($"{a} ");
+//     }
+//     Console.WriteLine();
+// }
+
+// void Clear(int[] array) 
+// {
+//     for (int i = 0; i < array.Length; i++) 
+//     {
+//         array[i] = 0;
+//     }
+// }
+
+// 30th test
+string status = "Healthy";
+
+Console.WriteLine($"Start: {status}");
+SetHealth(status, false);
+Console.WriteLine($"End: {status}");
+
+void SetHealth(string status, bool isHealthy) 
 {
-int diff = 0;
-if (Math.Abs(newGMT) > 12 || Math.Abs(currentGMT) > 12)
-{
-    Console.WriteLine("Invalid GMT");
+    status = (isHealthy ? "Healthy" : "Unhealthy");
+    Console.WriteLine($"Middle: {status}");
 }
-else if (newGMT <= 0 && currentGMT <= 0 || newGMT >= 0 && currentGMT >= 0) 
-{
-    diff = 100 * (Math.Abs(newGMT) - Math.Abs(currentGMT));
-} 
-else 
-{
-    diff = 100 * (Math.Abs(newGMT) + Math.Abs(currentGMT));
-}
-for (int i = 0; i < times.Length; i++) 
-{
-    int newTime = ((times[i] + diff)) % 2400;
-    Console.WriteLine($"{times[i]} -> {newTime}");
-}
-}
-DisplayAdjustedTimes(schedule, 6, -6);
