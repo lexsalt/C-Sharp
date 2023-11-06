990604a457379bbe5d527088161fac97c9bb21e2
lexsalt <emilianosal@hotmail.com>
2023-11-06 20:04:34 +0100

    31th test. Resume from macbook

--- Program.cs
+++ Program.cs
@@ -831,14 +831,46 @@ Console.WriteLine($"Last number: {current}");
 // }
 
 // 30th test
-string status = "Healthy";
+// string status = "Healthy";
 
-Console.WriteLine($"Start: {status}");
-SetHealth(status, false);
-Console.WriteLine($"End: {status}");
+// Console.WriteLine($"Start: {status}");
+// SetHealth(status, false);
+// Console.WriteLine($"End: {status}");
 
-void SetHealth(string status, bool isHealthy) 
-{
-    status = (isHealthy ? "Healthy" : "Unhealthy");
-    Console.WriteLine($"Middle: {status}");
-}
+// void SetHealth(string status, bool isHealthy) 
+// {
+//     status = (isHealthy ? "Healthy" : "Unhealthy");
+//     Console.WriteLine($"Middle: {status}");
+// }
+
+//31th test final chapter
+string[,] corporate = 
+	{
+		{"Robert", "Bavin"}, {"Simon", "Bright"},
+		{"Kim", "Sinclair"}, {"Aashrita", "Kamath"},
+		{"Sarah", "Delucchi"}, {"Sinan", "Ali"}};
+
+	string[,] external = 
+	{
+		{"Vinnie", "Ashton"}, {"Cody", "Dysart"},
+		{"Shay", "Lawrence"}, {"Daren", "Valdes"}
+	};
+
+	string externalDomain = "hayworth.com";
+
+	for (int i = 0; i < corporate.GetLength(0); i++) 
+	{
+		DisplayEmail(first: corporate[i,0], last: corporate[i,1]);
+	}
+
+	for (int i = 0; i < external.GetLength(0); i++) 
+	{
+		DisplayEmail(first: external[i,0], last: external[i,1], domain: externalDomain);
+	}
+
+	void DisplayEmail(string first, string last, string domain = "contoso.com") 
+	{
+		string email = first.Substring(0, 2) + last;
+		email = email.ToLower();
+		Console.WriteLine($"{email}@{domain}");
+	}
