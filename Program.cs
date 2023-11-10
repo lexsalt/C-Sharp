e156135bee897173aca5cddd97ea3cec169d913e
lexsalt <emilianosal@hotmail.com>
2023-11-10 15:56:29 +0100

    51th

--- Program.cs
+++ Program.cs
@@ -1400,53 +1400,64 @@ Console.WriteLine($"Last number: {current}");
 
 //50 test. string conversion methods built in
 
-string message = "(What if) I have [different symbols] but every {open symbol} needs a [matching closing symbol]?";
+// string message = "(What if) I have [different symbols] but every {open symbol} needs a [matching closing symbol]?";
 
-// The IndexOfAny() helper method requires a char array of characters. 
-// You want to look for:
+// // The IndexOfAny() helper method requires a char array of characters. 
+// // You want to look for:
 
-char[] openSymbols = { '[', '{', '(' };
+// char[] openSymbols = { '[', '{', '(' };
 
-// You'll use a slightly different technique for iterating through 
-// the characters in the string. This time, use the closing 
-// position of the previous iteration as the starting index for the 
-//next open symbol. So, you need to initialize the closingPosition 
-// variable to zero:
+// // You'll use a slightly different technique for iterating through 
+// // the characters in the string. This time, use the closing 
+// // position of the previous iteration as the starting index for the 
+// //next open symbol. So, you need to initialize the closingPosition 
+// // variable to zero:
 
-int closingPosition = 0;
+// int closingPosition = 0;
 
-while (true)
-{
-    int openingPosition = message.IndexOfAny(openSymbols, closingPosition);
+// while (true)
+// {
+//     int openingPosition = message.IndexOfAny(openSymbols, closingPosition);
 
-    if (openingPosition == -1) break;
+//     if (openingPosition == -1) break;
 
-    string currentSymbol = message.Substring(openingPosition, 1);
+//     string currentSymbol = message.Substring(openingPosition, 1);
 
-    // Now  find the matching closing symbol
-    char matchingSymbol = ' ';
+//     // Now  find the matching closing symbol
+//     char matchingSymbol = ' ';
 
-    switch (currentSymbol)
-    {
-        case "[":
-            matchingSymbol = ']';
-            break;
-        case "{":
-            matchingSymbol = '}';
-            break;
-        case "(":
-            matchingSymbol = ')';
-            break;
-    }
+//     switch (currentSymbol)
+//     {
+//         case "[":
+//             matchingSymbol = ']';
+//             break;
+//         case "{":
+//             matchingSymbol = '}';
+//             break;
+//         case "(":
+//             matchingSymbol = ')';
+//             break;
+//     }
 
-    // To find the closingPosition, use an overload of the IndexOf method to specify 
-    // that the search for the matchingSymbol should start at the openingPosition in the string. 
+//     // To find the closingPosition, use an overload of the IndexOf method to specify 
+//     // that the search for the matchingSymbol should start at the openingPosition in the string. 
 
-    openingPosition += 1;
-    closingPosition = message.IndexOf(matchingSymbol, openingPosition);
+//     openingPosition += 1;
+//     closingPosition = message.IndexOf(matchingSymbol, openingPosition);
 
-    // Finally, use the techniques you've already learned to display the sub-string:
+//     // Finally, use the techniques you've already learned to display the sub-string:
 
-    int length = closingPosition - openingPosition;
-    Console.WriteLine(message.Substring(openingPosition, length));
-}
+//     int length = closingPosition - openingPosition;
+//     Console.WriteLine(message.Substring(openingPosition, length));
+// }
+
+// 51th test
+
+string data = "12345John Smith          5000  3  ";
+string updatedData = data.Remove(5, 20);
+Console.WriteLine(updatedData);
+
+string message = "This--is--ex-amp-le--da-ta";
+message = message.Replace("--", " ");
+message = message.Replace("-", "");
+Console.WriteLine(message);
